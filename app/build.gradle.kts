c8a8deb40f17606cd6a7cdbc3a094ef52984d8b5
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-03-23 19:24:03 +0700

    up sdk version to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.myappbar"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myappbar"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
